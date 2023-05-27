8022070156097f4f967bc22d0cd51a089f87871f
kagamma <kagamma.km@gmail.com>
2023-05-27 14:08:22 +0700

    Backport from satania-buddy

--- ScriptEngine.pas
+++ ScriptEngine.pas
@@ -5351,7 +5351,7 @@ var
 
   function ParseFuncDecl(const IsAnon: Boolean = False): TSEToken;
   var
-    Token: TSEToken;
+    Token, TokenResult: TSEToken;
     Name: String;
     ArgCount: Integer = 0;
     I, FuncIndex: Integer;
@@ -5376,9 +5376,9 @@ var
       end;
       Result := Token;
 
-      Token.Value := 'result';
-      Token.Kind := tkIdent;
-      CreateIdent(ikVariable, Token, True);
+      TokenResult.Value := 'result';
+      TokenResult.Kind := tkIdent;
+      CreateIdent(ikVariable, TokenResult, True);
 
       NextTokenExpected([tkBracketOpen]);
       repeat
@@ -5399,6 +5399,8 @@ var
       FuncIndex := Self.FuncScriptList.Count - 1;
       ParentBinary := Self.Binary;
       Self.Binary := Self.VM.Binaries[Func^.BinaryPos];
+      if PeekAtNextToken.Kind = tkEqual then
+        Self.TokenList.Insert(Pos + 1, TokenResult);
       ParseBlock;
 
       ReturnList := ReturnStack.Pop;
