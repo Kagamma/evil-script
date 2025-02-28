dd76d7129ee7a67d0172f3a90112a8e9ad20b03f
Kagamma <7451778+Kagamma@users.noreply.github.com>
2025-02-28 21:21:19 +0800

    Update evil.pas

--- evil.pas
+++ evil.pas
@@ -9,21 +9,51 @@ uses
 var
   SE: TScriptEngine;
   SL: TStrings;
+  IsD: Boolean = False;
+  IsO: Boolean = True;
+  IsA: Boolean = False;
+  I: Integer;
+  AsmStr: String;
 
 begin
   if ParamCount < 1 then
   begin
-    Writeln('Usage: evil [script file]');
+    Writeln('Usage: evil <options> [script file]');
+    Writeln('Options: ');
+    Writeln(' -d  : Disassembly');
+    Writeln(' -do : Disable optimizations');
+    Writeln(' -da : Disable assertions');
     Halt;
   end;
+  if ParamCount > 1 then
+  begin
+    for I := 1 to ParamCount - 1 do
+      case ParamStr(I) of
+        '-d':
+          IsD := True;
+        '-do':
+          IsO := False;
+        '-da':
+          IsA := True;
+      end;
+  end;
   SE := TScriptEngine.Create;
-  SE.OptimizeAsserts := False;
+  SE.OptimizePeephole := IsO;
+  SE.OptimizeConstantFolding := IsO;
+  SE.OptimizeAsserts := IsA;
   SL := TStringList.Create;
   try
     try
-      SL.LoadFromFile(ParamStr(1));
+      SL.LoadFromFile(ParamStr(ParamCount));
       SE.Source := SL.Text;
-      SE.Exec;
+      if IsD then
+      begin
+        SE.Lex;
+        SE.Parse;
+        SEDisAsm(SE.VM, AsmStr);
+        Writeln(AsmStr);
+      end else
+        SE.Exec;
     except
      on E: Exception do
        Writeln(E.Message);
