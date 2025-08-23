29203d40e29859ebcb6af23db82f0abaf7446593
Kagamma <7451778+Kagamma@users.noreply.github.com>
2025-08-23 15:57:48 +0800

    Update bench

--- examples/bench/threads_local.py
+++ examples/bench/threads_local.py
@@ -1,15 +1,22 @@
 import threading as thr
 import time
 
+ga = 0
+gb = 0
+
 def incrA():
-  a=0
-  for i in range(200000000):
-    a+=1
+  a = 0
+  for i in range(200000001):
+    a += 1  
+  global ga
+  ga = a
 
 def incrB():
-  b=0
-  for i in range(200000000):
-    b+=1
+  b = 0
+  for i in range(200000001):
+    b += 1
+  global gb
+  gb = b
 
 def runThreads():
   thA=thr.Thread(target=incrA)
@@ -24,4 +31,6 @@ tBegin=time.time()
 runThreads()
 
 tEnd=time.time()
+
+print("a = ", ga, ", b = ", gb)
 print("Finished in : {}s".format(tEnd-tBegin))
