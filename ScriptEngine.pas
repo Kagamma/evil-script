d4127bf1b4f30e49a739a933bde031cbe563fb31
Kagamma <7451778+Kagamma@users.noreply.github.com>
2026-08-29 15:29:01 +0800

    +

--- ScriptEngine.pas
+++ ScriptEngine.pas
@@ -8227,19 +8227,23 @@ var
               E.MovRegFromSDXMM({$ifdef UNIX}regRSI{$else}regRDI{$endif}, TXMMReg(XMMStackPtr - 1));
               Dec(XMMStackPtr);
               //
-              //E.PushReg(regR15);
-              //E.PushReg(regR14);
-              //E.PushReg(regR13);
-              //E.PushReg(regR12);
-              //E.PushReg(regR10);
+              {$ifndef SE_DISABLE_AGGRESSIVE_JIT}
+                E.PushReg(regR15);
+                E.PushReg(regR14);
+                E.PushReg(regR13);
+                E.PushReg(regR12);
+                E.PushReg(regR10);
+              {$endif}
 
               E.CallAbsolute(regRCX, @SEMapGetJIT);
 
-              //E.PopReg(regR10);
-              //E.PopReg(regR12);
-              //E.PopReg(regR13);
-              //E.PopReg(regR14);
-              //E.PopReg(regR15);
+              {$ifndef SE_DISABLE_AGGRESSIVE_JIT}
+                E.PopReg(regR10);
+                E.PopReg(regR12);
+                E.PopReg(regR13);
+                E.PopReg(regR14);
+                E.PopReg(regR15);
+              {$endif}
 
               {$ifdef UNIX}
                 { Kind }
